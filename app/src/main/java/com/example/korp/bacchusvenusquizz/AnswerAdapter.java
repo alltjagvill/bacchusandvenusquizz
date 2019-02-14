6ebfd95e3d7ed258c53038ad7d561d31a391d4f9
Niclas Binninge <niclas.binninge@mysafety.se>
2019-02-14 14:44:48 +0100

    Cleanup

--- app/src/main/java/com/example/korp/bacchusvenusquizz/AnswerAdapter.java
+++ app/src/main/java/com/example/korp/bacchusvenusquizz/AnswerAdapter.java
@@ -2,14 +2,10 @@ package com.example.korp.bacchusvenusquizz;
 
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
--- app/src/main/java/com/example/korp/bacchusvenusquizz/ScoreActivity.java
+++ app/src/main/java/com/example/korp/bacchusvenusquizz/ScoreActivity.java
@@ -1,40 +1,32 @@
 package com.example.korp.bacchusvenusquizz;
 
 import android.content.Intent;
-import android.os.CountDownTimer;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
+
 
 public class ScoreActivity extends AppCompatActivity {
+
+    //Create arraylists
     ArrayList<String> rightAnswers;
     ArrayList<String> yourAnswers;
     ArrayList<String> questions;
-
     ArrayList<Answer> answerList = new ArrayList<>();
+    //****Create arraylists
 
+    //Gets back to category selection (prevents ordinary back press)
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(getApplicationContext(), genre.class);
         startActivity(intent);
         finish();
     }
+    //***Gets back to category selection (prevents back press
 
     int score = 0;
-    //int count = 0;
-
-
-
-
-
     TextView scoreCount;
 
     @Override
@@ -42,22 +34,22 @@ public class ScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
 
-
+//Get stuff from previous activity
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
 
         rightAnswers = extras.getStringArrayList("RIGHTANSWERS");
         yourAnswers = extras.getStringArrayList("YOURANSWERS");
         questions = extras.getStringArrayList("QUESTIONS");
-
-
-      //  ArrayList1 answerlist= (ArrayList1) extras.getSerializable("ANSWERLIST");
-
-     //   ArrayAdapter adapter = new AnswerAdapter(this, answerList);
-
         String headline = extras.getString("TITLE");
+        //***Get stuff from previous activity
+
+
+        //Sets headline of current quiz
         TextView title = findViewById(R.id.score_title);
         title.setText(headline);
+        //***Sets headline of current quiz
+
 
         String nrOfAnswers = Integer.toString(rightAnswers.size());
         TextView nrOfAnswersCount = findViewById(R.id.maxScore);
@@ -73,17 +65,11 @@ public class ScoreActivity extends AppCompatActivity {
 
     public void countScore() {
 
-        String questionString = getString(R.string.question);
-        String rightAnswerString = getString(R.string.rightAnswer);
-        String yourAnswerString = getString(R.string.yourAnswer);
         String right;
         String guessed;
         String question;
-        for (int i = 0; i < rightAnswers.size(); i++) {
 
-            //answerList.add(questionString + questions.get(i));
-            //answerList.add(rightAnswerString + rightAnswers.get(i));
-            //answerList.add(yourAnswerString + yourAnswers.get(i));
+        for (int i = 0; i < rightAnswers.size(); i++) {
 
             right = rightAnswers.get(i);
             guessed = yourAnswers.get(i);
@@ -93,40 +79,17 @@ public class ScoreActivity extends AppCompatActivity {
 
             answerList.add(answerObj);
 
-
-
-            //Log.d("SCORE", right);
-            //Log.d("SCORE", guessed);
-
             if (guessed.equals(right)) {
                 score++;
-
-
                 }
-
-
 
         }
 
         ListView answerView = findViewById(R.id.answerListView);
-        /*ArrayAdapter rightAnswerAdapter = new RightAnswerAdapter(this, rightAnswers);
-        ArrayAdapter yourAnswerAdapter = new YourAnswerAdapter(this, yourAnswers);
-        answerView.setAdapter(rightAnswerAdapter);
-        answerView.setAdapter(yourAnswerAdapter);*/
         AnswerAdapter adapter = new AnswerAdapter(this, answerList);
         answerView.setAdapter(adapter);
 
-
-        Log.d("SCORE", Integer.toString(score));
-        Log.d("SCORE1", String.valueOf(score));
-
         scoreCount.setText(String.valueOf(score));
-
-    }
-
-    public void scoreTimerCount() {
-
-
 
     }
 }
